c8967f0d662fa4fe80c6abf441c7a6723ab34dc5
Ali-0211-TR <xgamexarkon@gmail.com>
2024-09-21 14:57:28 +0500

    Save

--- presentation/api/src/server.rs
+++ presentation/api/src/server.rs
@@ -1,10 +1,21 @@
+use anyhow::Result;
+use dotenv::dotenv;
+
+use crate::controllers::app_routes;
+
 pub struct Server{
     port: u16,
 }
 
 impl Server{
     pub fn init() -> Self{
-        
+        dotenv().ok();
         Self { port: 3000 }
+    }
+    pub async fn run(self) -> Result<()>{
+        let addr = format!("0.0.0.0:{}", self.port);
+        let listener = tokio::net::TcpListener::bind(addr).await?;
+        axum::serve(listener, app_routes()).await?;
+        Ok(())
     }
 }
