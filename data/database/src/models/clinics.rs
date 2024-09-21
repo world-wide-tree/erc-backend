8fc6f0467a8c8d836562ab4b92fa5077fceed45f
Ali-0211-TR <xgamexarkon@gmail.com>
2024-09-21 23:07:54 +0500

    add db dto for clinics

--- data/database/src/models/clinics.rs
+++ data/database/src/models/clinics.rs
@@ -1,6 +1,9 @@
 use chrono::{DateTime, Utc};
+use domain::models::clinics::{ClinicModel, CreateClinicDto, CreateDoctorDto, CreatePatientDto, DoctorModel, PatientModel, UpdateClinicDto, UpdateDoctorDto, UpdatePatientDto};
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::Thing;
+
+use super::{CLINIC_TABLE, DOCTOR_TABLE, PATIENT_TABLE, SPECIALIZATION_TABLE, USER_TABLE};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct DoctorsDb{
@@ -39,7 +42,7 @@ pub struct ClinicsDb{
     address: String,
     contact_info: String,
     created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
+    updated_at: Option<DateTime<Utc>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -54,3 +57,159 @@ pub struct UpdateClinicDb{
     pub address: String,
     pub contact_info: String,
 }
+
+impl From<ClinicModel> for ClinicsDb{
+    fn from(value: ClinicModel) -> Self {
+        Self { 
+            id: Thing::from((CLINIC_TABLE, value.id.as_str())), 
+            name: value.name, 
+            address: value.address, 
+            contact_info: value.contact_info, 
+            created_at: value.created_at, 
+            updated_at: value.updated_at 
+        }
+    }
+}
+impl Into<ClinicModel> for ClinicsDb{
+    fn into(self) -> ClinicModel {
+        ClinicModel { 
+            id: self.id.id.to_string(), 
+            name: self.name, 
+            address: self.address, 
+            contact_info: self.contact_info, 
+            created_at: self.created_at, 
+            updated_at: self.updated_at 
+        }
+    }
+}
+impl From<DoctorModel> for DoctorsDb{
+    fn from(value: DoctorModel) -> Self {
+        Self { 
+            id: Thing::from((DOCTOR_TABLE, value.id.as_str())), 
+            user: Thing::from((USER_TABLE, value.user.as_str())), 
+            specialization: Thing::from((SPECIALIZATION_TABLE, value.specialization.as_str())), 
+            clinic: Thing::from((CLINIC_TABLE, value.clinic.as_str())) 
+        }
+    }
+}
+impl Into<DoctorModel> for DoctorsDb{
+    fn into(self) -> DoctorModel {
+        DoctorModel { 
+            id: self.id.id.to_string(), 
+            user: self.user.id.to_string(), 
+            specialization: self.specialization.id.to_string(), 
+            clinic: self.clinic.id.to_string() }
+    }
+}
+impl From<PatientModel> for PatientsDb{
+    fn from(value: PatientModel) -> Self {
+        Self { 
+            id: Thing::from((PATIENT_TABLE, value.id.as_str())), 
+            user: Thing::from((USER_TABLE, value.user.as_str())) 
+        }
+    }
+}
+impl Into<PatientModel> for PatientsDb{
+    fn into(self) -> PatientModel {
+        PatientModel { 
+            id: self.id.id.to_string(),
+            user: self.user.id.to_string() 
+        }
+    }
+}
+impl Into<CreateDoctorDto> for CreateDoctorDb {
+    fn into(self) -> CreateDoctorDto {
+        CreateDoctorDto { 
+            user: self.user.id.to_string(), 
+            specialization: self.specialization.id.to_string() 
+        }
+    }
+}
+impl Into<CreatePatientDto> for CreatePatientDb {
+    fn into(self) -> CreatePatientDto {
+        CreatePatientDto { 
+            user: self.user.id.to_string() 
+        }
+    }
+}
+impl Into<CreateClinicDto> for CreateClinicDb {
+    fn into(self) -> CreateClinicDto {
+        CreateClinicDto { 
+            name: self.name, 
+            address: self.address, 
+            contact_info: self.contact_info 
+        }
+    }
+}
+impl From<CreateDoctorDto> for CreateDoctorDb {
+    fn from(value: CreateDoctorDto) -> Self {
+        Self { 
+            user: Thing::from((USER_TABLE, value.user.as_str())), 
+            specialization: Thing::from((SPECIALIZATION_TABLE, value.specialization.as_str())) }
+    }
+}
+impl From<CreatePatientDto> for CreatePatientDb {
+    fn from(value: CreatePatientDto) -> Self {
+        Self { 
+            user: Thing::from((USER_TABLE, value.user.as_str())) }
+    }
+}
+impl From<CreateClinicDto> for CreateClinicDb {
+    fn from(value: CreateClinicDto) -> Self {
+        Self { 
+            name: value.name, 
+            address: value.address, 
+            contact_info: value.contact_info 
+        }
+    }
+}
+
+impl Into<UpdateDoctorDto> for UpdateDoctorDb {
+    fn into(self) -> UpdateDoctorDto {
+        UpdateDoctorDto { 
+            user: self.user.id.to_string(), 
+            specialization: self.specialization.id.to_string() 
+        }
+    }
+}
+impl Into<UpdatePatientDto> for UpdatePatientDb {
+    fn into(self) -> UpdatePatientDto {
+        UpdatePatientDto { 
+            user: self.user.id.to_string() 
+        }
+    }
+}
+impl Into<UpdateClinicDto> for UpdateClinicDb {
+    fn into(self) -> UpdateClinicDto {
+        UpdateClinicDto { 
+            name: self.name, 
+            address: self.address, 
+            contact_info: self.contact_info 
+        }
+    }
+}
+impl From<UpdateDoctorDto> for UpdateDoctorDb {
+    fn from(value: UpdateDoctorDto) -> Self {
+        Self { 
+            user: Thing::from((USER_TABLE, value.user.as_str())), 
+            specialization: Thing::from((SPECIALIZATION_TABLE, value.specialization.as_str())) }
+    }
+}
+impl From<UpdatePatientDto> for UpdatePatientDb {
+    fn from(value: UpdatePatientDto) -> Self {
+        Self { 
+            user: Thing::from((USER_TABLE, value.user.as_str())) }
+    }
+}
+impl From<UpdateClinicDto> for UpdateClinicDb {
+    fn from(value: UpdateClinicDto) -> Self {
+        Self { 
+            name: value.name, 
+            address: value.address, 
+            contact_info: value.contact_info 
+        }
+    }
+}
+
+
+
--- data/database/src/models/mod.rs
+++ data/database/src/models/mod.rs
@@ -3,3 +3,32 @@ pub mod clinics;
 pub mod diseases;
 pub mod relations;
 pub mod erc;
+
+
+pub const USER_TABLE: &'static str = "Users";
+pub const CLINIC_TABLE: &'static str = "Clinics";
+pub const DISEASES_TABLE: &'static str = "Diseases";
+pub const ERC_TABLE: &'static str = "Ercs";
+pub const DOCTOR_TABLE: &'static str = "Doctors";
+pub const PATIENT_TABLE: &'static str = "Patients";
+pub const SPECIALIZATION_TABLE: &'static str = "Specializations";
+
+pub const HAS_ERC_RELETION: &'static str = "HasErcRelation";
+pub const VISITS_RELETION: &'static str = "VisitsRelation";
+pub const HAS_DOCTORS_RELETION: &'static str = "HasDoctorsRelation";
+pub const TREATS_RELETION: &'static str = "TreatsRelation";
+pub const SPECIALIZATION_IN_RELETION: &'static str = "SpecializationInRelation";
+pub const HAS_HSITORY_RELETION: &'static str = "HasHistoryRelation";
+pub const DIAGNOSED_BY_RELETION: &'static str = "DiagnosedByRelation";
+pub const TREATED_IN_RELETION: &'static str = "TreatedInRelation";
+pub const CONTAINS_HISTORY_RELETION: &'static str = "ContainsHistoryRelation";
+
+
+
+
+
+
+
+
+
+
